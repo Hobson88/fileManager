431db27f8f7b9064e7b527c33ed4d0597b430436
Hobson88 <krawczakrobert@gmail.com>
2019-03-07 11:41:32 +0100

    Add in POM.XML junit.jupiter-api, -params, -engine.
    Add FileManagerJsonTest

---
+++ src/test/java/pl/sda/FileManagerJsonTest.java
@@ -0,0 +1,15 @@
+package pl.sda;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class FileManagerJsonTest {
+    @DisplayName(
+            ""
+    )
+    @Test
+    void test0 (){
+
+    }
+}
